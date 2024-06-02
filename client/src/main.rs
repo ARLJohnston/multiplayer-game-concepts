3d65e6a5db06e4eef77e1ed6c583f6d69861ebdd
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-06-02 21:35:38 +0100

    feat(client): add basic bevy sprite

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,29 @@
 use json::object;
 use std::net::UdpSocket;
 
+use bevy::prelude::*;
+
+fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle {
+			camera: Camera {
+		    clear_color: ClearColorConfig::Custom(Color::hex("2f2f2f").unwrap()),
+			  ..default()
+		  },
+			..default() //Fill remaining args with default values
+	  });
+
+    let texture = asset_server.load("lucy.png");
+
+    commands.spawn(SpriteBundle {
+				sprite: Sprite {
+		        custom_size: Some(Vec2::new(100.0, 100.0)),
+		        ..default()
+				},
+				texture,
+		    ..default()
+	  });
+}
+
 fn main() {
     let json_instance = object!{
 			"code": 200,
@@ -19,4 +42,9 @@ fn main() {
     let serialised_json: &[u8] = string_json.as_bytes();
 
     socket.send_to(serialised_json, target).expect("Unable to send data to {target}");
+
+    App::new()
+	    .add_plugins(DefaultPlugins)
+			.add_systems(Startup, setup_camera)
+			.run();
 }
