03464549aaf1e28b7d9ff09b0a89eb508791cc0d
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-06-05 17:51:23 +0100

    feat(client): correctly receive json data

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,28 @@
-use json::object;
+use serde_json::json;
+use serde_json::Value;
 use std::net::UdpSocket;
 
 use bevy::prelude::*;
 
-fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn character_movement(mut characters: Query<(&mut Transform, &Sprite)>, input: Res<ButtonInput<KeyCode>>, time: Res<Time>,) {
+    for (mut transform, _) in &mut characters {
+				if input.pressed(KeyCode::KeyW) {
+		        transform.translation.y += 100.0 * time.delta_seconds()
+				}
+				if input.pressed(KeyCode::KeyS) {
+		        transform.translation.y -= 100.0 * time.delta_seconds()
+				}
+				if input.pressed(KeyCode::KeyA) {
+		        transform.translation.x -= 100.0 * time.delta_seconds()
+				}
+				if input.pressed(KeyCode::KeyD) {
+		        transform.translation.x += 100.0 * time.delta_seconds()
+				}
+
+	  }
+}
+
+fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>,) {
     commands.spawn(Camera2dBundle {
 			camera: Camera {
 		    clear_color: ClearColorConfig::Custom(Color::hex("2f2f2f").unwrap()),
@@ -24,27 +43,45 @@ fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
 	  });
 }
 
+fn setup_framelimit(mut settings: ResMut<bevy_framepace::FramepaceSettings>) {
+    use bevy_framepace::Limiter;
+    settings.limiter = Limiter::from_framerate(30.0)
+}
+
 fn main() {
-    let json_instance = object!{
-			"code": 200,
-				success: true,
-				payload: {
-	    epic: true
-		}
-    };
-    println!("Json: {json_instance}");
+    let target = "127.0.0.1:5050";
+	  let socket = UdpSocket::bind("127.0.0.1:6969").expect("Couldn't connect to {target}");
 
-    let sself: &str = "localhost:5051";
-    let target: &str = "localhost:5050";
-	let socket = UdpSocket::bind(sself).expect("Couldn't connect to {target}");
+		let json = json!({
+				"guid" : "15234y",
+				"position" : {
+          "x": 10.0,
+          "y": 26.0
+        },
+    });
 
-    let string_json = json::stringify(json_instance);
-    let serialised_json: &[u8] = string_json.as_bytes();
 
-    socket.send_to(serialised_json, target).expect("Unable to send data to {target}");
+		// let string_json = json::stringify(json_instance);
+		// let serialised_json: &[u8] = json_instance.as_bytes();
+
+		socket.send_to(json.to_string().as_bytes(), target).expect("Unable to send data to {target}");
+    let mut buf = vec![0; 1024];
+
+    let (len, src) = socket.recv_from(&mut buf).expect("recv function failed");
+
+    let recv_json: Value = serde_json::from_slice(&buf[..len]).expect("Unable to decode json");
+
+    println!("{}", recv_json);
+
 
     App::new()
-	    .add_plugins(DefaultPlugins)
+			.add_plugins(DefaultPlugins
+				   .set(ImagePlugin::default_nearest())
+					 .build(),
+      )
+			.add_plugins(bevy_framepace::FramepacePlugin)
 			.add_systems(Startup, setup_camera)
+			.add_systems(Startup, setup_framelimit)
+			.add_systems(Update, character_movement)
 			.run();
 }
