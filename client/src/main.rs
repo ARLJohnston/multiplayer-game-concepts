059338b716d85ba7ce2d0795d6b322bb5610d775
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-06-30 16:16:49 +0100

    feat(client): make client report changes at reasonable rate

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,14 @@
 use serde_json::json;
 use serde_json::Value;
 use std::net::UdpSocket;
+use std::time::Duration;
 
 use bevy::prelude::*;
 
 #[derive(Resource)]
 pub struct GameState {
     socket: UdpSocket,
+    timer: Timer,
     x: f32,
     y: f32,
 }
@@ -15,8 +17,10 @@ impl Default for GameState {
     fn default() -> Self {
         let socket = UdpSocket::bind("127.0.0.1:6969").expect("Couldn't connect to {target}");
         socket.set_nonblocking(true).unwrap();
+
         GameState {
             socket,
+            timer: Timer::new(Duration::from_secs(1), TimerMode::Repeating),
             x: 0.,
             y: 0.,
         }
@@ -25,25 +29,11 @@ impl Default for GameState {
 
 fn character_movement(
     mut characters: Query<(&mut Transform, &Sprite)>,
-    mut gameState: Res<GameState>,
+    mut game_state: ResMut<GameState>,
     input: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
 ) {
     for (mut transform, _) in &mut characters {
-        let json = json!({
-                    "guid" : "15234y",
-                    "position" : {
-              "x": transform.translation.x, //Start with this as we only want to update from server when we get it
-              "y": transform.translation.y
-            },
-        });
-        let socket = &gameState.socket;
-        let target = "127.0.0.1:5050";
-
-        socket
-            .send_to(json.to_string().as_bytes(), target)
-            .expect("Unable to send data to {target}");
-
         if input.pressed(KeyCode::KeyW) {
             transform.translation.y += 100.0 * time.delta_seconds()
         }
@@ -57,20 +47,30 @@ fn character_movement(
             transform.translation.x += 100.0 * time.delta_seconds()
         }
 
+        let socket = &game_state.socket;
         let mut buf = vec![0; 1024];
-
         match socket.recv_from(&mut buf) {
             Ok((len, _src)) => {
                 let recv_json: Value =
                     serde_json::from_slice(&buf[..len]).expect("Unable to decode json");
 
+                println!("{}", serde_json::to_string_pretty(&recv_json).unwrap());
+
+                match recv_json["position"]["x"].as_f64() {
+                    Some(val) => transform.translation.x = val as f32,
+                    _ => (),
+                }
+
                 match recv_json["position"]["y"].as_f64() {
-                    Some(val) => transform.translation.y += val as f32,
+                    Some(val) => transform.translation.y = val as f32,
                     _ => (),
                 }
             }
             _ => (),
         }
+
+        game_state.x = transform.translation.x;
+        game_state.y = transform.translation.y;
     }
 }
 
@@ -95,6 +95,26 @@ fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
     });
 }
 
+fn server_report(time: Res<Time>, mut game_state: ResMut<GameState>) {
+    game_state.timer.tick(time.delta());
+    if game_state.timer.finished() {
+        let socket = &game_state.socket;
+        let target = "127.0.0.1:5050";
+
+        let json = json!({
+                    "guid" : "15234y",
+                    "position" : {
+              "x": game_state.x,
+              "y":game_state.y,
+            },
+        });
+
+        socket
+            .send_to(json.to_string().as_bytes(), target)
+            .expect("Unable to send data to {target}");
+    }
+}
+
 fn setup_framelimit(mut settings: ResMut<bevy_framepace::FramepaceSettings>) {
     use bevy_framepace::Limiter;
     settings.limiter = Limiter::from_framerate(30.0)
@@ -108,5 +128,6 @@ fn main() {
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, setup_framelimit)
         .add_systems(Update, character_movement)
+        .add_systems(Update, server_report)
         .run();
 }
